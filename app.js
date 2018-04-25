e342b58c14235391db2a32fbdf6d070cb3cbd3cd
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-04-25 11:01:12 -0400

    Add files via upload
     commit to grade

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const port = 2000;
 app.use(express.urlencoded({extended: true}));
 
 app.post('/', function(req, res) {
-  var name = req.body.user_name;
-  var mail1 = req.body.user_mail1;
+  var name = req.body.user_name, name;
+  var mail1 = req.body.user_mail1, mail1;
   var mail2 = req.body.user_mail2;
   var homepage = req.body.homepage;
   var method = req.body.user_method;
   var device = req.body.user_device;
+  var inquiry = req.body.user_inquiry;
+  var plan  = req.body.user_plan;
+  var message = req.body.user_messge;
 
 //   var email = req.body.user_mail;
 
@@ -19,12 +22,15 @@ app.post('/', function(req, res) {
   console.log("mail1: " + mail1);
   console.log("mail2: " + mail2);
   console.log("homepage: " + homepage);
-  console.log("method: " + user_device + value);
-  console.log("device: " + user_devoce +value);
+  console.log("method: " + method);
+  console.log("device: " + device );
+  console.log("inquiry:" + inquiry);
+  console.log("plan" + plan + ' ');
+  console.log("message" + message + ' ');
 
   res.send(`
     <h1>Thanks ${name}!</h1>
-    <p>We received your message below, and will get back to you at <strong>${email}</strong>.</p>
+    <p>We received your message below, and will get back to you at <strong>${mail1}</strong>.</p>
     <blockquote>${message}</blockquote>
     `);
 });
